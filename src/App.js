f552cdb1f983ac14d54eaac4e11979212df4356b
Chris Ham <431647+greenham@users.noreply.github.com>
2020-04-17 05:16:32 -0700

    basic shit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
+import { race, racers, comms } from "./config";
+import { ReactComponent as TwitchGlitch } from "./TwitchGlitchWhite.svg";
+import { ReactComponent as CommsIcon } from "./comms.svg";
+
+const raceStyle = racers.length > 2 ? "grid" : "headsup";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h2 className="race-title">{race.title}</h2>
+        <h3 className="race-subtitle">{race.subtitle}</h3>
       </header>
+
+      <main>
+        <section id={raceStyle}>
+          <div className="container">
+            {racers.map((racer) => (
+              <div className="racer-container">
+                <div className="game-feed"></div>
+                <div className="racer-nameplate">
+                  <TwitchGlitch
+                    className="text-icon"
+                    style={{ paddingRight: 5 }}
+                  />{" "}
+                  <span className="racer-name">{racer.name.toUpperCase()}</span>
+                </div>
+                <div className="racer-final-time">{racer.finalTime}</div>
+              </div>
+            ))}
+          </div>
+        </section>
+
+        {comms.length > 0 && (
+          <section id={"comms-" + raceStyle}>
+            <CommsIcon className="text-icon" />
+            {comms.map((comm) => (
+              <div className="comm-name">{comm.name}</div>
+            ))}
+          </section>
+        )}
+      </main>
+
+      <footer>
+        <small>Presented by ForeverGrind.fm</small>
+      </footer>
     </div>
   );
 }
---
+++ src/config.js
@@ -0,0 +1,26 @@
+const race = {
+  title: "All Dungeons RMG",
+  subtitle: "2020 Tournament - WB Round 2",
+};
+
+const racers = [
+  {
+    name: "Osse101 (8)",
+    finalTime: "1:18:26",
+  },
+  {
+    name: "daotmb (9)",
+    finalTime: "1:13:11",
+  },
+];
+
+const comms = [
+  {
+    name: "mkosler",
+  },
+  {
+    name: "mooglemod",
+  },
+];
+
+export { race, racers, comms };
