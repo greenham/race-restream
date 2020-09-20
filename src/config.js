0ea9253e31589fbf5eabdbfb8714489c7e536d8d
greenham <greenham83@gmail.com>
2020-09-20 10:54:59 -0700

    initial import

--- src/config.js
+++ src/config.js
@@ -1,26 +1,26 @@
 const race = {
-  title: "All Dungeons RMG",
-  subtitle: "2020 Tournament - WB Round 2",
+  title: "Any% No Major Glitches",
+  subtitle: "Germench Tournament",
 };
 
 const racers = [
   {
-    name: "Osse101 (8)",
-    finalTime: "1:18:26",
+    name: "KainDeschain",
+    finalTime: "",
   },
   {
-    name: "daotmb (9)",
-    finalTime: "1:13:11",
+    name: "ErirorPS",
+    finalTime: "",
   },
 ];
 
 const comms = [
-  {
-    name: "mkosler",
-  },
-  {
-    name: "mooglemod",
-  },
+  // {
+  //   name: "mkosler",
+  // },
+  // {
+  //   name: "mooglemod",
+  // },
 ];
 
 export { race, racers, comms };
