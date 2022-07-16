eddf700ea646e0ca4c6ab4e5aebe233013ce4d36
greenham <greenham83@gmail.com>
2022-07-16 07:16:37 -0700

    single config, update for nmg league

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Suspense } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import config from "./config-g3rd0-chex.json";
+import config from "./config.json";
 const DefaultTheme = React.lazy(() => import("./themes/default/DefaultTheme"));
 const NMG2021Theme = React.lazy(() =>
   import("./themes/nmg-tourney-2021/NMG2021Theme")
